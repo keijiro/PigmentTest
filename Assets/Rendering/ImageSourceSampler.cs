3d923c76cecfa8385968b62e849fa4492d913133
Keijiro Takahashi <keijiro@gmail.com>
2025-08-01 15:26:24 +0900

    Directory structure

---
+++ Assets/Rendering/ImageSourceSampler.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using Klak.TestTools;
+
+public sealed class ImageSourceSampler : MonoBehaviour
+{
+    [field:SerializeField] public float Interval { get; set; } = 0.5f;
+
+    [SerializeField] ImageSource _source = null;
+    [SerializeField] RenderTexture _destination = null;
+
+    async void Start()
+    {
+        while (true)
+        {
+            Graphics.Blit(_source.AsTexture, _destination);
+            await Awaitable.WaitForSecondsAsync(Interval);
+        }
+    }
+}
